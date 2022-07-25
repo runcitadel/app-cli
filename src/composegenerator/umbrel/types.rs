8a7982bfea3b30ce5f7114e58bf43c6445f589ba
AaronDewes <aaron.dewes@protonmail.com>
2022-07-25 19:47:07 +0000

    More cleanups

--- src/composegenerator/umbrel/types.rs
+++ src/composegenerator/umbrel/types.rs
@@ -1,6 +1,9 @@
 use serde::{Serialize, Deserialize};
+#[cfg(feature = "schema")]
+use schemars::JsonSchema;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "schema", derive(JsonSchema))]
 #[serde(rename_all = "camelCase")]
 pub struct Metadata {
     /// The version of the metadata file
